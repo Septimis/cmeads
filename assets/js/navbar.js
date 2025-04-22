2977262d317a354b329c19365e50ca4597ad58c9
Connor Meads <connor@cmeads.com>
2025-04-22 14:11:27 -0600

    Navbar is now fully responsive to window resizing and various screen sizes

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -2,22 +2,34 @@
 const NAV_HIGHLIGHT = document.getElementById('nav-highlight');
 NAV_HIGHLIGHT.style.height = `${document.getElementsByClassName("nav-item")[0].getBoundingClientRect().height}px`;
 
+let active_nav_item = null;
+
+// Move the highlighted widget when the window resizes
+window.addEventListener('resize', place_nav_highlight);
+
+// Sets the highlighted widget where the active navigation item is
+function place_nav_highlight()
+{
+	NAV_HIGHLIGHT.style.left = `${active_nav_item.offsetLeft}px`;
+	NAV_HIGHLIGHT.style.top = `${active_nav_item.offsetTop}px`;
+	NAV_HIGHLIGHT.style.width = `${active_nav_item.getBoundingClientRect().width}px`;
+}
+
+// Handle displaying the correct content according to what NavItem was clicked.
+// Simulates the NavItem getting highlighted
 function OnNavItemClicked(NavItem, ContentID)
 {
-	const CONTENT = document.getElementById(ContentID);
+	active_nav_item = NavItem;
 
 	// Hide all content before displaying the new
 	for(let element of document.getElementsByClassName('page-content'))
 	{
 		element.style.display = 'none';
 	}
+	document.getElementById(ContentID).style.display = 'block';
 
 	// Move the navigation highlight element to hover over the appropriate element
-	NAV_HIGHLIGHT.style.left = `${NavItem.offsetLeft}px`;
-	NAV_HIGHLIGHT.style.top = `${NavItem.offsetTop}px`;
-	NAV_HIGHLIGHT.style.width = `${NavItem.getBoundingClientRect().width}px`;
-
-	CONTENT.style.display = 'block';
+	place_nav_highlight();
 }
 
 // Simulate a click on the first nav element on script load
